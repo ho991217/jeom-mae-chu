dc5d4f3ef9645c7f98847e70f95a884849562b7a
이호연 <ho991217@kakao.com>
2024-04-16 13:45:05 +0900

    feat: 베트남 음식 추가

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,19 +3,22 @@ export * from './korean';
 export * from './japanese';
 export * from './chinese';
 export * from './western';
+export * from './vietnamese';
 
 import { 한식, Korean } from './korean';
 import { 중식, Chinese } from './chinese';
 import { 일식, Japanese } from './japanese';
 import { 양식, Western } from './western';
+import { 베트남, Vietnamese } from './vietnamese';
 
 const foods = {
   한식,
   중식,
   일식,
   양식,
+  베트남,
 };
 
-export type Food = Korean | Chinese | Japanese | Western;
+export type Food = Korean | Chinese | Japanese | Western | Vietnamese;
 
 export default foods;
