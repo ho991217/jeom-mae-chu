3ed4902e06af208b752c89bfaa62b3cdb42c97d6
이호연 <ho991217@kakao.com>
2024-04-16 14:36:45 +0900

    feat: 카테고리 추가

--- src/data/category.ts
+++ src/data/category.ts
@@ -1,3 +1,3 @@
-export const category = ['한식', '중식', '일식', '양식', '베트남'] as const;
+export const category = ['한식', '중식', '일식', '양식', '베트남', '디저트'] as const;
 
 export type Category = (typeof category)[number];
